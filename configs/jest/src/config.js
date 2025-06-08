624deac6cdcaf698b9a793ff2fedc4ee06e18c38
guesung <gueit214@naver.com>
2025-06-08 17:30:28 +0900

    fix: 빌드 에러

--- configs/jest/src/config.js
+++ configs/jest/src/config.js
@@ -1,14 +1,14 @@
 const defaultSettings = {
+  testEnvironment: "jest-fixed-jsdom",
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest"],
+  },
   moduleNameMapper: {
     "^.+\\.svg$": "jest-svg-transformer",
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
     "@/(.*)": "<rootDir>/src/$1",
   },
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js", "<rootDir>/jest.setup.ts"],
-  testPathIgnorePatterns: ["/node_modules/", "/dist/", "/esm/"],
-  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-  moduleDirectories: ["node_modules", "src"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
 
 function settings(extraSettings) {
