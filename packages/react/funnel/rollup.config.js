5d923b8758269c9a8c520f0d35b2963df99562ad
guesung <gueit214@naver.com>
2025-06-08 17:49:06 +0900

    chore: 공통 jest, rollup 설정으로 수정

--- packages/react/funnel/rollup.config.js
+++ packages/react/funnel/rollup.config.js
@@ -1,33 +1,7 @@
-const resolve = require("@rollup/plugin-node-resolve");
-const commonjs = require("@rollup/plugin-commonjs");
-const typescript = require("@rollup/plugin-typescript");
-const peerDepsExternal = require("rollup-plugin-peer-deps-external");
-const terser = require("@rollup/plugin-terser");
-
-const packageJson = require("./package.json");
-
-const isProduction = process.env.NODE_ENV === "production";
+const { config } = require("@guesung/rollup-config");
 
 module.exports = {
-  input: "src/index.ts",
-  output: [
-    {
-      file: packageJson.main,
-      format: "cjs",
-      sourcemap: !isProduction,
-    },
-    {
-      file: packageJson.module,
-      format: "esm",
-      sourcemap: !isProduction,
-    },
-  ],
-  plugins: [
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    typescript({ tsconfig: "./tsconfig.json" }),
-    terser(),
-  ],
-  external: ["react", "react-dom"],
+  ...config({
+    packageJson: require("./package.json"),
+  }),
 };
