e98f33abff6dfb7484d8f10f908038872c17eba1
guesung <gueit214@naver.com>
2025-06-12 17:25:43 +0900

    feat: Query 예시 코드

--- examples/react/query/src/App.tsx
+++ examples/react/query/src/App.tsx
@@ -2,7 +2,14 @@ import "./App.css";
 
 import { useQuery } from "@guesung/query";
 
-const fetchJsonData = async (): Promise<{ title: string }> => {
+interface Response {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+const fetchJsonData = async (): Promise<Response> => {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
   return response.json();
 };
@@ -12,11 +19,19 @@ function App() {
     queryKey: "test",
     queryFn: fetchJsonData,
     initialData: {
-      title: "test",
+      userId: 0,
+      id: 0,
+      title: "",
+      body: "",
     },
   });
 
-  return <div>s</div>;
+  return (
+    <div>
+      <h4>제목 : {data?.title}</h4>
+      <p>{data?.body}</p>
+    </div>
+  );
 }
 
 export default App;
