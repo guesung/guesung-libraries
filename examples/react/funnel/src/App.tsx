34ec757532e50d89a562a3236d64892e32538ff6
guesung <gueit214@naver.com>
2025-06-12 14:50:57 +0900

    feat: Funnel 예시 코드 작성

--- examples/react/funnel/src/App.tsx
+++ examples/react/funnel/src/App.tsx
@@ -1,18 +1,51 @@
-import { useState } from "react";
 import "./App.css";
 
-import { Funnel } from "@guesung/funnel";
+import { Funnel, useFunnelContext } from "@guesung/funnel";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  console.log(Funnel);
-
   return (
-    <>
-      <Funnel></Funnel>
-    </>
+    <Funnel>
+      <Funnel.Step index={1}>
+        <Step1 />
+      </Funnel.Step>
+      <Funnel.Step index={2}>
+        <Step2 />
+      </Funnel.Step>
+      <Funnel.Step index={3}>
+        <Step3 />
+      </Funnel.Step>
+    </Funnel>
   );
 }
 
 export default App;
+
+function Step1() {
+  const { goNextStep } = useFunnelContext();
+  return (
+    <div>
+      <h1>Step 1</h1>
+      <button onClick={goNextStep}>Next</button>
+    </div>
+  );
+}
+
+function Step2() {
+  const { goNextStep } = useFunnelContext();
+  return (
+    <div>
+      <h1>Step 2</h1>
+      <button onClick={goNextStep}>Next</button>
+    </div>
+  );
+}
+
+function Step3() {
+  const { resetStep } = useFunnelContext();
+  return (
+    <div>
+      <h1>Step 3</h1>
+      <button onClick={resetStep}>Reset</button>
+    </div>
+  );
+}
