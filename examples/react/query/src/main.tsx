b0086afdb2558615b0c34368ea15b35051d36026
guesung <gueit214@naver.com>
2025-06-16 17:24:43 +0900

    chore: 단언문 제거하고 분기 처리

--- examples/react/query/src/main.tsx
+++ examples/react/query/src/main.tsx
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const container = document.getElementById("root");
+if (!container) throw new Error("#root element not found");
+
+ReactDOM.createRoot(container).render(
 	<React.StrictMode>
 		<Suspense fallback={<div>로딩 중</div>}>
 			<App />
